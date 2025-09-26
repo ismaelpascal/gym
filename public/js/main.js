9ef6338c95eeb937c4eef25ed343be1e5ea781e7
ismaelpascal <ismaelmolina856@gmail.com>
2025-09-25 21:01:32 -0300

    nose adivina

--- public/js/main.js
+++ public/js/main.js
@@ -22,7 +22,10 @@ function mostrarClientes(lista) {
           <div class="space-x-2">
             <button class="bg-yellow-400 hover:bg-yellow-700 text-white text-xs py-1 px-3 rounded-md duration-300" onclick="editarCliente(${index})">Editar</button>
             <button class="bg-red-500 hover:bg-red-700 text-white text-xs py-1 px-3 rounded-md duration-300" onclick="eliminarCliente(${index})">Eliminar</button>
-            <button class="bg-green-500 hover:bg-green-700 text-white text-xs py-1 px-3 rounded-md duration-300" onclick="verPagos(${index})">Ver Pagos</button>
+            <label class="flex items-center gap-1">
+                <input type="checkbox" class="w-4 h-4 text-green-600 border-gray-300 rounded focus:ring-green-500">
+                <span class="text-sm text-gray-700">Pago</span>
+              </label>
           </div>
         </td>
       `;
